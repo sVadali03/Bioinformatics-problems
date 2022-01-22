df29c1ffa446e6c944dcf8f908706f3e0897f788
Supriya Vadali <66241504+sVadali03@users.noreply.github.com>
2022-01-22 17:54:26 +0530

    Add files via upload

---
+++ Python Village/problem 1.py
@@ -0,0 +1,5 @@
+a = 892
+b = 889
+
+c_squared = a**2 + b**2
+print(c_squared)
---
+++ Python Village/problem 2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 22 17:45:53 2022
+
+@author: Supri
+"""
+
+string = '''CvkLLy8SJLlYIk31s18nnFx3yP8kWcpIYJywGjIcOjpiNWIrK1p9Wa5hpEGwuw9f81VNAixXITjnJSQ3cobhth4yp1GcKjVnvgyEAgmFDSm6BTvOyqQqUhY6HWWEmoGYKPMGRXl6AGsrpqk6EbMgZZGyGzWW4TW0rHdFsNkm9btY47F6japonica6U5y.
+'''
+
+a,b,c,d = 68,70,176,183
+
+
+print(string[a:b+1],string[c:d+1])
